bf4c7da2ae6bee9c1d419e5e05f061f8d0a04ea6
Cody <zimquo@gmail.com>
2024-02-08 14:27:50 -0700

    Maps

--- JS_Review_Learn/variables.js
+++ JS_Review_Learn/variables.js
@@ -63,3 +63,55 @@ const five = "Five";
 integers.push(one, two, three, four, five);
 console.log("My array: ", integers); // My array: [1, 2, 3, 4, 5, "One", "Two", "Three", "Four", "Five"]
 
+
+
+// maps
+/*
+-> key-value pairs
+-> keys can be any type
+-> values can be any type
+-> keys are unique
+-> remember insertion order
+*/
+let map = new Map();
+map["name"] = "John"; // not a key-value pair
+console.log(map.has("name")); // false
+
+let user = new Map();
+user.set("name", "John");
+console.log(user.has("name")); // true
+
+let users = new Map();
+users.set("Dave", {age: 25, height: 5.5});
+console.log(users.has("Dave")); // true
+console.log(users); // Map { 'Dave' => { age: 25, height: 5.5 } }
+console.log(users.get("Dave")); // { age: 25, height: 5.5 }
+users.set("Sally", {healthy: true, location: "USA"});
+console.log(users); // Map { 'Dave' => { age: 25, height: 5.5 }, 'Sally' => { healthy: true, location: 'USA' } }
+console.log(users.size); // 2
+users.delete("Dave");
+console.log(users); // Map { 'Sally' => { healthy: true, location: 'USA' } }
+users.clear();
+console.log(users); // Map {}
+
+map.set({name: "John", Age: 25}, {healthy: true, location: "USA"});
+console.log(map); // Map { Object => Object }
+console.log(map.get({name: "John", Age: 25})); // undefined
+for (let [key, value] of map) {
+    console.log(key, value);
+}   // { name: 'John', Age: 25 } { healthy: true, location: 'USA' }
+
+// challenge
+/*
+Store these in a map:
+key:{name:"Yellow", identity:"Color"} value: "Red"
+key:{name:"Jake",Age:15} value: "Blue"
+key:{Health:"Bad",identity:"Smoker"} value: {Age:100}
+*/
+let myMap = new Map();
+myMap.set({name:"Yellow", identity:"Color"}, "Red");
+myMap.set({name:"Jake",Age:15}, "Blue");
+myMap.set({Health:"Bad",identity:"Smoker"}, {Age:100});
+console.log(myMap); // Map { Object => 'Red', Object => 'Blue', Object => { Age: 100 } }
+
+
